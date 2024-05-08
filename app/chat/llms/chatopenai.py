febff87f690d5acea189952a7d687857c45e96c2
samnags <sam.nagourney@gmail.com>
2024-05-08 10:37:51 -0400

    Refactored maps

--- app/chat/llms/chatopenai.py
+++ app/chat/llms/chatopenai.py
@@ -1,7 +1,7 @@
 from langchain.chat_models import ChatOpenAI
 
-def build_llm(chat_args):
+def build_llm(chat_args, model_name):
     return ChatOpenAI(
         streaming=chat_args.streaming,
-        model_name="gpt-4"
+        model_name=model_name
     )
